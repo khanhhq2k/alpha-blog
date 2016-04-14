5f4ff2f09845f024ae4b539f66aefbf3571784c1
Khanh <khanhhq2k@gmail.com>
2016-04-14 10:04:43 +0000

    add SessionsController and session new view to login, add routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,11 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :articles
-  resources :users, except: [:new] 
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   root 'pages#home'
   get '/about', to: 'pages#about'
   get 'signup', to: 'users#new'
