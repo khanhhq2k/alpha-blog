948a282b4b3aa42d093cfc27f4dbbb52ce2226c0
Khanh <khanhhq2k@gmail.com>
2016-04-14 20:53:47 +0000

    simple log in log out(part 1), add log out button to nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,22 @@ class SessionsController < ApplicationController
       
   end
   def create
-   #render :new
+    #assign user if there is an existing emails
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #notice: user.authenticate will return the user object if password is match,if not, return False(not sure!!)
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in!"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Wrong email or password, please try again!"
+      render 'new'
+    end
    
   end
   def destroy
-  
+    session[:user_id] = nil
+    flash[:success] = "You have logged out!"
+    redirect_to root_path
   end
 end
