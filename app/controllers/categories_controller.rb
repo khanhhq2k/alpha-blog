8b7b471e728fbb7594a5fb22b9f3c472d52615b9
Khanh <khanhhq2k@gmail.com>
2016-04-23 17:59:18 +0000

    add validation in CategoryCOntroller so that only admin can create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  
+  before_action :require_admin, except: [:index, :show]
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -25,5 +25,12 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+    def require_admin
+      #not loggin=> out, loggin but the user is not admin=> out!!!
+      if !logged_in? || (logged_in? && !current_user.admin?)
+        flash[:danger] = "Only admin can perform this action!"
+        redirect_to categories_path
+      end
     end
 end
